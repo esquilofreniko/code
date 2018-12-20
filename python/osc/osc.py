269c8e76844a3db64d17fcbf7b3ed8366a70ce1d
esquilofreniko <esquilofreniko@gmail.com>
2018-12-20 02:21:03 +0000

    python-osc working

--- python/osc/osc.py
+++ python/osc/osc.py
@@ -11,46 +11,53 @@ from pythonosc import osc_server
 from pythonosc import osc_message_builder
 from pythonosc.osc_message_builder import OscMessageBuilder
 
-def oscInit(port):
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default="127.0.0.1")
-    parser.add_argument("--port", type=int, default=port)
-    args = parser.parse_args()
-    client = udp_client.SimpleUDPClient(args.ip, args.port)
-    return client
+class OscClient:
+    def __init__(self,ip,port):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--ip", default=ip)
+        parser.add_argument("--port", type=int, default=port)
+        args = parser.parse_args()
+        self.client = udp_client.SimpleUDPClient(args.ip, args.port)
 
-def build_osc_message(positioning_values):
-    builder = OscMessageBuilder(address='/wek/inputs')
-    for v in positioning_values:
-        builder.add_arg(v)
-    return builder.build()
+    def build_osc_message(self,positioning_values):
+        builder = OscMessageBuilder(address='/wek/inputs')
+        for v in positioning_values:
+            builder.add_arg(v)
+        return builder.build()
 
-def print_filter_handler(unused_addr, *args):
-    x = args
-    print(x)
+    def sendMsg(self,msg):
+        out = self.build_osc_message(msg)
+        self.client.send(out)
 
-dispatcher = dispatcher.Dispatcher()
-dispatcher.map("/filter", print_filter_handler)
-server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 4000), dispatcher)
-print("Serving on {}".format(server.server_address))
-server_thread = threading.Thread(target=server.serve_forever)
-server_thread.start()
-client = oscInit(57120)
+
+class OscServer:
+    def print_msg(self,unused_addr, *args):
+        self.msg = args
+        print(self.msg)
+        
+    def __init__(self,ip,port,handler):
+        self.dispatcher = dispatcher.Dispatcher()
+        self.dispatcher.map(handler, self.print_msg)
+        self.server = osc_server.ThreadingOSCUDPServer((ip, port), self.dispatcher)
+        print("OSC server listening on {}".format(self.server.server_address),"handler:",handler)
+        server_thread = threading.Thread(target=self.server.serve_forever)
+        server_thread.start()
 
 # Main Loop
+oscserver = OscServer("127.0.0.1",4000,"/test")
+oscclient = OscClient("127.0.0.1",57120)
 print("press q to quit...")
 while True:
-    msg = build_osc_message(np.random.rand(15))
-    client.send(msg)
+    oscclient.sendMsg(np.random.rand(15))
     time.sleep(0.05)
     try: 
         if keyboard.is_pressed('q'): 
-            server.shutdown()
+            oscserver.server.shutdown()
             quit()
             break 
         else:
             pass
     except:
-        server.shutdown()
+        oscserver.server.shutdown()
         quit()
         break 
